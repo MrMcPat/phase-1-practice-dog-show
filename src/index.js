e1b9ced3e50d0dbb4b0d97b2c12312e6ce076ecc
Patrick Liang <pl204511194@yahoo.com>
2022-01-19 21:04:46 -0500

    commit tree a3383edf798f39cb019d0f4883fa8cd0080e1546

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-document.addEventListener('DOMContentLoaded', () => {
+function fetchDogs() {
+    return fetch("http://localhost:3000/dogs")
+    .then(resp => resp.json())
+    .then(data => {
+        const tableBody = document.getElementById("table-body");
+        console.log(data);
+        data.forEach(dog => {
+            const doggo = document.createElement("tr");
+            doggo.innerHTML = `
+            <td>${dog.name}</td>
+            <td>${dog.breed}</td>
+            <td>${dog.sex}</td>
+            <td><button id="${dog.id}">Edit</button></td>
+            `
+            tableBody.appendChild(doggo);
+        })
+    })
+}
 
+function editDogs(event) {
+    event.preventDefault();
+    let dogObj = {
+        name: event.target.elements[0].value,
+        breed: event.target.elements[1].value,
+        sex: event.target.elements[2].value
+    }
+    document.getElementById("table-body").addEventListener("click", event => {
+        patchDogs(dogObj, event.target.id);
+    }, {once: true})
+}
+
+function patchDogs(dogObj, dog) {
+    return fetch(`http://localhost:3000/dogs/${dog}`,{
+        method: "PATCH",
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dogObj),
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchDogs();
+    document.getElementById("dog-form").addEventListener("submit", editDogs);
 })
